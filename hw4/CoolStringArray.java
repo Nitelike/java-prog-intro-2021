3880d4dfc8168b72930de4feb005cd81342637e4
Nitelike <ax.2904@gmail.com>
2021-09-28 16:43:40 +0300

    replaced tabs with spaces (ver. 2.1)

--- hw4/CoolStringArray.java
+++ hw4/CoolStringArray.java
@@ -3,30 +3,30 @@ package homework;
 import java.util.Arrays;
 
 public class CoolStringArray {
-	private String[] arr;
-	private int length;
+    private String[] arr;
+    private int length;
 
-	public CoolStringArray() {
-		arr = new String[0];
-		length = 0;
-	}
+    public CoolStringArray() {
+        arr = new String[0];
+        length = 0;
+    }
 
-	public void add(String val) {
-		if (length >= arr.length) {
-			arr = Arrays.copyOf(arr, (arr.length*3/2) + 1);	
-		}
-		arr[length++] = val;
-	}
+    public void add(String val) {
+        if (length >= arr.length) {
+            arr = Arrays.copyOf(arr, (arr.length*3/2) + 1);    
+        }
+        arr[length++] = val;
+    }
 
-	public String get(int ind) {
-		return arr[ind];
-	}
+    public String get(int ind) {
+        return arr[ind];
+    }
 
-	public void set(int ind, String val) {
-		arr[ind] = val;
-	}
+    public void set(int ind, String val) {
+        arr[ind] = val;
+    }
 
-	public int length() {
-		return length;
-	}
+    public int length() {
+        return length;
+    }
 }
--- hw4/WordStatInput.java
+++ hw4/WordStatInput.java
@@ -4,94 +4,90 @@ import homework.CoolStringArray;
 import java.nio.charset.StandardCharsets;
 
 public class WordStatInput {
-	public static void main(String[] args) {
-		String inputName = "input.txt", outputName = "output.txt";
-		CoolStringArray input = new CoolStringArray();
+    public static void main(String[] args) {
+        String inputName = "input.txt", outputName = "output.txt";
+        CoolStringArray input = new CoolStringArray();
 
-		try {
-			inputName = args[0];
-			outputName = args[1];
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.err.println("Not all arguments were provided. Can't set right filenames.");
-		}
+        try {
+            inputName = args[0];
+            outputName = args[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Not all arguments were provided. Can't set right filenames.");
+        }
 
-		try {
-			BufferedReader in = new BufferedReader(new FileReader(inputName, StandardCharsets.UTF_8));
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(inputName, StandardCharsets.UTF_8));
 
-			try {
-				String line = in.readLine();
+            try {
+                String line = in.readLine();
 
-				while (line != null) {
-					line = line.toLowerCase() + " ";
-					int leftPtr = 0;
+                while (line != null) {
+                    line = line.toLowerCase() + " ";
+                    int leftPtr = 0;
 
-					for (int i = 0; i < line.length(); i++) {
-						char c = line.charAt(i);
-						if (!canBeInWord(c)) {
-							if (i - leftPtr >= 1) {
-								String word = line.substring(leftPtr, i);
-								input.add(word);
-							}
-							leftPtr = i+1;
-						}
-					}
+                    for (int i = 0; i < line.length(); i++) {
+                        char c = line.charAt(i);
+                        if (!canBeInWord(c)) {
+                            if (i - leftPtr >= 1) {
+                                String word = line.substring(leftPtr, i);
+                                input.add(word);
+                            }
+                            leftPtr = i+1;
+                        }
+                    }
 
-					line = in.readLine();
-				}
-			} finally {
-				in.close();
-			}
-		} catch (FileNotFoundException e) {
-			System.err.println("Can't find " + inputName);
-		} catch (IOException e) {
-			System.err.println("Can't read from " + inputName);
-		}
+                    line = in.readLine();
+                }
+            } finally {
+                in.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Can't find " + inputName);
+        } catch (IOException e) {
+            System.err.println("Can't read from " + inputName);
+        }
 
-		try {
-			BufferedWriter out = new BufferedWriter(new FileWriter(outputName, StandardCharsets.UTF_8));
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(outputName, StandardCharsets.UTF_8));
 
-			try {
-				Integer[] indexes = new Integer[input.length()];
-				int[] cnt = new int[input.length()];
+            try {
+                Integer[] indexes = new Integer[input.length()];
+                int[] cnt = new int[input.length()];
 
-				for (int i = 0; i < indexes.length; i++) {
-					indexes[i] = i;
-				}
-				Arrays.sort(indexes, new Comparator<Integer>() {
-				    public int compare(Integer o1, Integer o2) {
-				        return input.get(o2).compareTo(input.get(o1));
-				    }
-				});
+                for (int i = 0; i < indexes.length; i++) {
+                    indexes[i] = i;
+                }
+                Arrays.sort(indexes, (a, b) -> input.get(a).compareTo(input.get(b)));
 
-				for(int i = 0; i < indexes.length; i++) {
-					int j = i;
-					int minInd = indexes[i];
-					while(j < indexes.length && input.get(indexes[i]).equals(input.get(indexes[j]))) {
-						minInd = Math.min(minInd, indexes[j]);
-						j++;
-					}
-					cnt[minInd] = j-i;
-					i = j-1;
-				}
+                for(int i = 0; i < indexes.length; i++) {
+                    int j = i;
+                    int minInd = indexes[i];
+                    while(j < indexes.length && input.get(indexes[i]).equals(input.get(indexes[j]))) {
+                        minInd = Math.min(minInd, indexes[j]);
+                        j++;
+                    }
+                    cnt[minInd] = j-i;
+                    i = j-1;
+                }
 
-				for(int i = 0; i < input.length(); i++) {
-					if(cnt[i] > 0) {
-						out.write(input.get(i) + " " + cnt[i]);
-					    out.newLine();
-					}
-				}
+                for(int i = 0; i < input.length(); i++) {
+                    if(cnt[i] > 0) {
+                        out.write(input.get(i) + " " + cnt[i]);
+                        out.newLine();
+                    }
+                }
 
-			} finally {
-				out.close();
-			}
-		} catch (FileNotFoundException e) {
-			System.err.println("Can't find " + outputName);
-		} catch (IOException e) {
-			System.err.println("Can't write to " + outputName);
-		}
-	}
+            } finally {
+                out.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Can't find " + outputName);
+        } catch (IOException e) {
+            System.err.println("Can't write to " + outputName);
+        }
+    }
 
-	private static boolean canBeInWord(char c) {
-		return (Character.isLetter(c) || Character.getType(c) == Character.DASH_PUNCTUATION || c == '\'');
-	}
+    private static boolean canBeInWord(char c) {
+        return (Character.isLetter(c) || Character.getType(c) == Character.DASH_PUNCTUATION || c == '\'');
+    }
 }
