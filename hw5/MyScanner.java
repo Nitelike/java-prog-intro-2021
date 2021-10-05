84684ff9406e4ef8760f7a8edb597b06ed9254fb
Nitelike <ax.2904@gmail.com>
2021-10-05 19:31:06 +0300

    hw5 ver 6.0

--- hw5/MyScanner.java
+++ hw5/MyScanner.java
@@ -1,6 +1,6 @@
 package homework;
 
-import java.io.BufferedReader;
+import java.io.Reader;
 import java.io.InputStreamReader;
 import java.io.FileReader;
 import java.io.StringReader;
@@ -8,31 +8,64 @@ import java.io.File;
 import java.io.InputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import homework.CoolArray;
 import java.util.function.Predicate;
+import java.nio.charset.StandardCharsets;
 
 public class MyScanner {
-    private BufferedReader in;
+    private final int BUFFER_SIZE = 1024;
+    private char[] buffer = new char[BUFFER_SIZE];
+    private int bufferedChars = 0;
+    private int ptr = 0;
+    private Reader in;
 
     public MyScanner(InputStream source) {
-        in = new BufferedReader( new InputStreamReader(source));
+        in = new InputStreamReader(source);
     }
 
     public MyScanner(File source) throws FileNotFoundException, IOException {
-        in = new BufferedReader(new FileReader(source, StandardCharsets.UTF_8));
+        in = new FileReader(source, StandardCharsets.UTF_8);
     }
 
     public MyScanner(String source) {
-        in = new BufferedReader(new StringReader(source));
+        in = new StringReader(source);
     }
 
     public int nextChar() throws IOException {
-        return in.read();
+        if (ptr >= bufferedChars && bufferedChars >= 0) {
+            bufferedChars = in.read(buffer);
+            ptr = 0;
+        }
+        if (ptr < bufferedChars) {
+            return (int)buffer[ptr++];
+        }
+        return -1;
     }
 
     public String nextLine() throws IOException {
-        return in.readLine();
+        String res = null;
+        CoolArray buf = new CoolArray();
+        int x = nextChar();
+
+        while (true) {
+            if (x == -1) {
+                break;
+            }
+            char c = (char)x;
+            res = "";
+            if (c != '\n' && c != '\r') {
+                buf.add(c);
+            } else if (c == '\n') {
+                break;
+            }
+            x = nextChar();
+        }
+
+        if (buf.length() > 0) {
+            res = buf.toString();
+        }
+
+        return res;
     }
 
     public String next() throws IOException {
